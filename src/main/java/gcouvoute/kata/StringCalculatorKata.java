6159122ac941e9bdd834b18b70732834aef128c9
gcouvoute <guillaume.couvoute@gmail.com>
2021-12-23 14:54:17 +0100

    Implement add method to match the requirements of the 5 first steps

--- src/main/java/gcouvoute/kata/StringCalculatorKata.java
+++ src/main/java/gcouvoute/kata/StringCalculatorKata.java
@@ -1,8 +1,48 @@
 package gcouvoute.kata;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+
 public class StringCalculatorKata {
 
     int add(String numbers) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (StringUtils.isEmpty(numbers)) {
+            return 0;
+        }
+
+        char delimiter = ',';
+        String chars = numbers;
+
+        if (numbers.matches("^//.\n[\\S\\s]*$")) {
+            if (numbers.length() == 4) {
+                return 0;
+            }
+            delimiter = numbers.charAt(2);
+            chars = numbers.substring(4);
+        }
+
+        List<Integer> negatives = new ArrayList<>();
+
+        Integer sum = Arrays.stream(chars.split("[\n" + delimiter + "]"))
+                .map(Integer::parseInt)
+                .map(integer -> {
+                    if (integer < 0) {
+                        negatives.add(integer);
+                    }
+                    return integer;
+                })
+                .reduce(0, Integer::sum);
+
+        if (negatives.isEmpty()) {
+            return sum;
+        } else {
+            StringJoiner joiner = new StringJoiner(", ");
+            negatives.forEach(integer -> joiner.add(integer.toString()));
+            throw new IllegalArgumentException("negatives not allowed: " + joiner);
+        }
     }
 }
